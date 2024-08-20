09a23db89ff4c2fdac10bb49bf149434d7eb938d
Niza Toshpulatov <nizomiddin.toshpulatov@synotgames.com>
2024-08-20 07:52:24 +0200

    Remove spell autocomplete, add autocmd to close oil float with esc key

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,15 @@
 return {
-  "nvimtools/none-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local null_ls = require("null-ls")
-    local prettier = require("utils").is_windows() and "prettier" or "prettierd"
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local null_ls = require("null-ls")
+		local prettier = require("utils").is_windows() and "prettier" or "prettierd"
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting[prettier],
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.completion.spell,
-      },
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting[prettier],
+				null_ls.builtins.formatting.stylua,
+			},
+		})
+	end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,20 +1,28 @@
 return {
   "stevearc/oil.nvim",
   lazy = false,
-  opts = {
-    float = {
-      override = function(conf)
-        conf.row = 0
-        conf.col = 9999
-        conf.width = 48
-        conf.height = 16
-
-        return conf
-      end,
-    },
-  },
   keys = {
-    { "<leader>e", "<cmd>lua require('oil').open_float()<cr>", desc = "Open Oil in float" },
-    { "<leader>E", "<cmd>Oil<cr>",                             desc = "Open Oil in parent directory" },
+    { "<leader>e", "<cmd>lua require('oil').toggle_float()<cr>", desc = "Open Oil in float" },
+    { "<leader>E", "<cmd>Oil<cr>",                               desc = "Open Oil in parent directory" },
   },
+  config = function()
+    require("oil").setup({
+      float = {
+        override = function(config)
+          config.row = 0
+          config.col = 9999
+          config.width = 48
+          config.height = 16
+
+          return config
+        end,
+      },
+    })
+
+    vim.cmd([[
+      augroup oil_escape
+        autocmd FileType oil nnoremap <buffer> <esc> <cmd>lua require('oil').close()<cr>
+      augroup END
+    ]])
+  end,
 }
