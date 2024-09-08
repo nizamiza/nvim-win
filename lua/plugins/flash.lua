79b352f56aee015b42e21e54e1500606fc10d36c
Niza Toshpulatov <niza@hey.com>
2024-09-08 22:58:59 +0200

    Format

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,18 +2,8 @@ return {
   "folke/flash.nvim",
   opts = {},
   keys = {
-    {
-      "s",
-      "<cmd>lua require('flash').jump()<cr>",
-      desc = "Flash",
-      mode = { "n", "x", "o" },
-    },
-    {
-      "S",
-      "<cmd>lua require('flash').treesitter()<cr>",
-      desc = "Flash Treesitter",
-      mode = { "n", "x", "o" },
-    },
+    { "s",     "<cmd>lua require('flash').jump()<cr>",              desc = "Flash",               mode = { "n", "x", "o" } },
+    { "S",     "<cmd>lua require('flash').treesitter()<cr>",        desc = "Flash Treesitter",    mode = { "n", "x", "o" } },
     { "r",     "<cmd>lua require('flash').remote()<cr>",            desc = "Remote Flash",        mode = "o" },
     { "R",     "<cmd>lua require('flash').treesitter_search()<cr>", desc = "Treesitter search",   mode = { "x", "o" } },
     { "<c-s>", "<cmd>lua require('flash').toggle()<cr>",            desc = "Toggle Flash search", mode = "c" },
